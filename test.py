0ba39589e942215c5223250b75470a1272201e95
buhuu <i21102@yuge.kosen-ac.jp>
2023-12-13 00:18:04 +0900

    simple graph

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import numpy as np
-data = pd.read_csv("weight.csv",keep_default_na=False)
-data.date()
-#data.plot(x = 'date',y = 'weight')
+import matplotlib.pyplot as plt
+#data = pd.read_csv("weight.csv",keep_default_na=False)
+
+df = pd.read_csv("weight.csv")
+matrix2 = df[df.columns[0]]
+matrix1 = df[df.columns[1]]
+list2 = matrix2.tolist()
+list1 = matrix1.tolist()
+
+x_axis = list2
+y_axis = list1
+plt.plot(x_axis,y_axis)
+plt.title('title name')
+plt.xlabel('x_axis name')
+plt.ylabel('y_axis name')
+plt.show()
