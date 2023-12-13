18d3503c99abdacfdfd78d3069ad18b98ee443af
buhuu <i21102@yuge.kosen-ac.jp>
2023-12-13 10:01:35 +0900

    matplot oo tusdad n gargav

--- test.py
+++ test.py
@@ -4,15 +4,28 @@ import matplotlib.pyplot as plt
 #data = pd.read_csv("weight.csv",keep_default_na=False)
 
 df = pd.read_csv("weight.csv")
-matrix2 = df[df.columns[0]]
-matrix1 = df[df.columns[1]]
-list2 = matrix2.tolist()
-list1 = matrix1.tolist()
 
-x_axis = list2
-y_axis = list1
-plt.plot(x_axis,y_axis)
-plt.title('Weight Loss Graph')
-plt.xlabel('Weight/kg')
-plt.ylabel('Date')
-plt.show()
+matrix1 = df[df.columns[0]]
+matrix2 = df[df.columns[1]]
+
+list1 = matrix2.tolist()
+list2 = matrix1.tolist()
+x = pd.DataFrame(list1)
+new = x.fillna(method='ffill', limit=100)
+list1 = new.values.tolist()
+print(list1)
+BMI = []
+
+# for i in list1:
+#     BMI.append(i/(1.64*1.64))
+# print(BMI)
+
+
+# while j<8:
+#     sum = 0
+#     for i in list1:
+#         sum = sum(i)
+#         count = count + 1
+#         if count == 7:
+#             continue
+#     print(sum)
